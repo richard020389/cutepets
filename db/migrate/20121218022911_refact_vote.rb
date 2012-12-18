1a451f5913bb2416e450cb80b4cc9d7f590fe916
richard020389 <richard020389@gmail.com>
2012-12-18 15:47:12 +0800

    fix migration error

--- db/migrate/20121218022911_refact_vote.rb
+++ db/migrate/20121218022911_refact_vote.rb
@@ -2,12 +2,12 @@ class RefactVote < ActiveRecord::Migration
   def up
     add_column :votes, :direct, :string, default:"up"
     add_column :votes, :user_id, :integer
-    remove_column :votes, :vote_count, :integer
+    remove_column :votes, :vote_count
   end
 
   def down
-    remove_column :votes, :direct, :string
-    remove_column :votes, :user_id, :integer
+    remove_column :votes, :direct
+    remove_column :votes, :user_id
     add_column :votes, :vote_count, :integer, :default => 0
   end
 end
