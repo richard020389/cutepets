472d5c858f80a00aeb36a4e7c1c00980a6fae622
richard020389 <richard020389@gmail.com>
2012-12-19 14:59:26 +0800

    add twitter handle to user profile

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,6 @@
 class User < ActiveRecord::Base
-  attr_accessible :username,:password
-
+  attr_accessible :username,:password,:twitter_handle
   has_secure_password
-
   validates :username,uniqueness:true
 
   has_many :pets
